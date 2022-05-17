eab2ba0f266686b8bc3c860cf974b1f0bc841746
Kevin Bell <bellKevin@pm.me>
2022-05-17 02:08:50 +0000

    Update FindHypotenuse.java

--- findHypotenuse/src/findhypotenuse/FindHypotenuse.java
+++ findHypotenuse/src/findhypotenuse/FindHypotenuse.java
@@ -1,23 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package findhypotenuse;
-
 import java.util.Scanner;
-
-/**
- *
- * @author 4800590195
- */
 public class FindHypotenuse {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         Scanner computerKeyboardInput = new Scanner(System.in);
         double base, height, hypotenuse;
         System.out.print("Enter right triangle base: ");
